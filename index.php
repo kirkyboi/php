8d50f871115f62ff492181bee86fc5c89785dc66
kirkyboi <136046535+kirkyboi@users.noreply.github.com>
2024-10-21 17:45:39 +0800

    index.php

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+  echo "<h1>Welcome to Our Veterinary Clinic</h1>";
+  echo "<p>We provide top-notch care for your pets. Explore our services to learn more.</p>";
+  echo "<a href='about.php'>About Us</a><br>";
+  echo "<a href='services.php'>Our Services</a><br>";
+  echo "<a href='contact.php'>Contact Us</a>";
+?>
