a6c3759c501eb3901897ffd407397b7ec492c644
kirkyboi <136046535+kirkyboi@users.noreply.github.com>
2024-10-21 17:47:55 +0800

    services.php

---
+++ services.php
@@ -0,0 +1,9 @@
+<?php
+  echo "<h1>Our Services</h1>";
+  echo "<ul>
+          <li>Pet Grooming</li>
+          <li>Veterinary Consultations</li>
+          <li>Pet Boarding</li>
+          <li>Pet Training</li>
+        </ul>";
+?>
